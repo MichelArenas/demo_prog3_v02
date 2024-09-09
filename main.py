69a9e8ef2b1667b47c7baa807ce43ad8b7541395
MichelArenas <michel.arenas28572@ucaldas.edu.co>
2024-09-09 10:56:06 -0500

    implementacion resta

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 def suma(x,y):
     return x+y
+
 respuesta = suma(5,7)
 print(respuesta)
+
+def resta(x,y):
+    return x-y
+
+respuesta = suma(5,7)
+print (respuesta)
+respuesta = respuesta(7-5)
+print("la respuesta es:",respuesta)
