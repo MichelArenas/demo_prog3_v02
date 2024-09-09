89be90bcb0afa29abadec8dc6773e9c9851d9755
MichelArenas <michel.arenas28572@ucaldas.edu.co>
2024-09-09 11:05:38 -0500

    eliminacion de suma

--- main.py
+++ main.py
@@ -1,13 +1,4 @@
-def suma(x,y):
-    return x+y
-
-respuesta = suma(5,7)
-print(respuesta)
-
 def resta(x,y):
     return x-y
-
-respuesta = suma(5,7)
-print (respuesta)
-respuesta = respuesta(7-5)
+respuesta = resta(7-5)
 print("la respuesta es:",respuesta)
